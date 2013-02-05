b72214a305ce8fe833afd4aca72f1fb2f0a64619
Andreas Stührk <andy-python@hammerhartes.de>
2013-02-05 23:51:23 +0100

    Added some request methods to the example app.

--- docs/example_app/example/views.py
+++ docs/example_app/example/views.py
@@ -13,6 +13,8 @@ def view_page(request):
 def add_page(request):
     "Add a new wiki page."
 
-@view_config(route_name='edit_page', renderer='templates/edit.pt')
+@view_config(
+    route_name='edit_page', renderer='templates/edit.pt',
+    request_method=("GET", "POST"))
 def edit_page(request):
     "Edit an existing wiki page."
